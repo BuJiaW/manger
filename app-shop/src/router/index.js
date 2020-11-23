e6880a0466a07b8e0aea23ec2df9ecad8decbf57
bujiawei <bujiawei@example.com>
2020-11-23 14:20:20 +0800

    项目基本配置

--- app-shop/src/router/index.js
+++ app-shop/src/router/index.js
@@ -1,28 +1,83 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Layout from '../components/Layout'
 
 Vue.use(VueRouter);
 
 const routes = [
+
+  // 登录
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/login',
+    name: "login",
+    component: () => import("../views/login"),
   },
+  // 首页
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: '/',
+    name: "Layout",
+    component: Layout,
+    redirect: '/index',
+    children: [
+      {
+        path: '/index',
+        component: () => import("../views/index"),
+      }
+    ]
+  },
+
+  // 会员管理
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: 'menber',
+        component: () => import("../views/merber"),
+      }
+    ]
+  },
+
+  // 供应商管理
+  {
+    path: '/supplier',
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        component: () => import("../views/supplier"),
+      }
+    ]
+  },
+
+  // 商品管理
+  {
+    path: '/goods',
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        component: () => import("../views/goods"),
+      }
+    ]
+  },
+
+  // 员工管理
+  {
+    path: '/staff',
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        component: () => import("../views/staff"),
+      }
+    ]
+  },
+
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: "hash",
   base: process.env.BASE_URL,
   routes
 });
